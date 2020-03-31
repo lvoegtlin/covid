f101612be85e76d8c0eca20218c4f475c75bc88d
Salim Hermidas <salim@hermidas.ch>
2020-03-31 19:23:15 +0200

    -- nice animated scroll

--- website/js/covid.js
+++ website/js/covid.js
@@ -1,5 +1,30 @@
 $(document).ready(function() {
+
     loadReports();
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 50) {
+            $('#back-to-top').fadeIn();
+        } else {
+            $('#back-to-top').fadeOut();
+        }
+    });
+
+    $('#back-to-top').click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 'slow');
+        return false;
+    });
+
+    $('.anchor').on('click', function(e) {
+        e.preventDefault();
+        var target = $($(this).attr('href'));
+        $('html,body').animate({
+                scrollTop: target.offset().top
+            }, 'slow');
+    });
+
 });
 
 function loadReports() {
